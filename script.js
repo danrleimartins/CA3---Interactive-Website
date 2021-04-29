b6acda4b93b9c31a68d8bfd7b58b8c18939fcf41
Danrlei Martins <danrlei.martins@gmail.com>
2021-04-29 22:11:35 +0100

    Food Menu Changes

--- script.js
+++ script.js
@@ -33,15 +33,15 @@ $('#user-gen').on('click', getData);
 
         users.forEach(function(api){
             output += `
+                <br>
                 <div> 
-                        <ul> 
-                            <li><h2>Name: ${api.name.title} ${api.name.first} ${api.name.last}</h2></li>
-                            <li> <img src="${api.picture.large}"></li>
-                            <li><h2>Phone Number: ${api.cell}</h2></li>
-                            <li><h2>Age: ${api.dob.age}</h2></li>
-                            <li><h2>Email: ${api.email}</h2></li>
-                            <li><h2>City: ${api.location.city}</h2></li>
-                            <li><h2>Country: ${api.location.country}</h2></li>
+                        <ul class="list-unstyled"> 
+                            <li><h3>${api.name.title} ${api.name.first} ${api.name.last}</h3></li>
+                            <li> <img class="img-user" src="${api.picture.large}"></li>
+                            <li>Age: ${api.dob.age}</li>
+                            <li>Phone Number: ${api.cell}</li>
+                            <li>Email: ${api.email}</li>
+                            <li>Location: ${api.location.city}, ${api.location.country} </li>
                         </ul>
                 <hr></div>`;
         });
